df3b9995488b33864e36517c41edc8da95a0a013
sumitsawant1029 <sumitsawant1029@gmail.com>
2025-12-04 15:00:57 +0530

    Make Navbar

---
+++ app/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <nav className='navbar'>
+                <div>
+                    A1CMSS
+                </div>
+                <div>
+                    <button> Home </button>
+                    <button> About Us </button>
+                    <button> Contact Us </button>
+                   
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
