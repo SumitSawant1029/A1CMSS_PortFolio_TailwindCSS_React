cbc0551a0798a2c33054975f7e10fd151fb824f9
Sumit <sumitsawant1029@gmail.com>
2025-12-07 23:32:50 +0530

    Done with basic navbar

--- app/src/Components/Navbar.jsx
+++ app/src/Components/Navbar.jsx
@@ -1,18 +1,24 @@
 import './Navbar.css';
+import logo from '../assets/logo-removebg.png';
 
 function Navbar() {
     return (
         <>
             <nav className='navbar'>
                 <div>
-                    A1CMSS
+                    <img src={logo} style={{ width: "5vw" }} />
                 </div>
-                <div>
-                    <button> Home </button>
-                    <button> About Us </button>
-                    <button> Contact Us </button>
-                   
+                <div className='social'>
+                    <button type="button" class="btn btn-outline-primary">Home</button>
+                    <button type="button" class="btn btn-outline-primary">Contact</button>
+                    <button type="button" class="btn btn-outline-primary">About Us</button>
                 </div>
+                <div className='social'>
+                    <a class="fa-brands fa-facebook"  style={{color:"white",textDecoration:"none"}}></a>
+                    <a class="fa-brands fa-instagram" style={{color:"white" ,textDecoration:"none"}}></a> 
+                    <a class="fa-brands fa-linkedin" style={{color:"white" ,textDecoration:"none"}}></a>
+                </div>
+
             </nav>
         </>
     )
